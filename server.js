34fbcce90d2963437beae1a3ffd29bf2f2c4ceac
Justin B. Watson <justin@brimelive.com>
2021-09-27 03:54:02 -0500

    cleanup

--- server.js
+++ server.js
@@ -34,17 +34,10 @@ nms.on('preConnect', (id, args) => {
   // session.reject();
 });
 
-nms.on('postConnect', (id, args) => {
-  console.log('[IngestEvent on postConnect]', `id=${id} args=${JSON.stringify(args)}`);
-});
-
-nms.on('doneConnect', (id, args) => {
-  console.log('[IngestEvent on doneConnect]', `id=${id} args=${JSON.stringify(args)}`);
-});
-
 nms.on('prePublish', (id, StreamPath, args) => {
 let body = {
     "ingest_id": id,
+    "timestamp": Date.now(),
     "region": process.env.region,
     "server_ip": process.env.server_ip,
     "channel": StreamPath.replace('/live/',''),
@@ -52,7 +45,7 @@ let body = {
 }
 console.log(body)
  // let session = nms.getSession(id);
- //session.reject();
+ // session.reject();
 });
 
 nms.on('postPublish', (id, StreamPath, args) => {
@@ -61,18 +54,4 @@ nms.on('postPublish', (id, StreamPath, args) => {
 
 nms.on('donePublish', (id, StreamPath, args) => {
   console.log('[IngestEvent on donePublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
-});
-
-nms.on('prePlay', (id, StreamPath, args) => {
-  console.log('[IngestEvent on prePlay]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
-  // let session = nms.getSession(id);
-  // session.reject();
-});
-
-nms.on('postPlay', (id, StreamPath, args) => {
-  console.log('[IngestEvent on postPlay]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
-});
-
-nms.on('donePlay', (id, StreamPath, args) => {
-  console.log('[IngestEvent on donePlay]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
 });
