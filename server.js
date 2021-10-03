c5b569204d9dd1fcd923d4b45d751b6c823b96b6
Justin B. Watson <justin@brimelive.com>
2021-10-03 05:46:24 -0500

    stuff

--- server.js
+++ server.js
@@ -2,13 +2,47 @@ const NodeMediaServer = require('node-media-server');
 require('dotenv').config();
 const axios = require('axios');
 
+async function checkSessionShit({id, args, streamPath}){
+  // check here and return false if you want to reject
+  let data = {
+    name: streamPath.split('/').pop(),
+    auth: args.auth,
+    id
+  }
+  console.log(data)
+  const sleep = ()=>new Promise(async (res, rej)=>{
+    axios.post('https://api.brime.tv/v1/account/stream_auth', data).then(function (response) {
+      if(response.status === 200){
+        return res('yey')
+      }
+    })
+    .catch(function (error) {
+      rej('err')
+    });
+    // setTimeout(()=>{
+    //   if(Math.random() < 0.65){
+    //     return res('yey')
+    //   }
+    //   rej('err')
+    // }, 250)
+  })
+
+  try{
+    await sleep()
+    return true
+  }catch(err){
+    return false
+  }
+}
+
 const config = {
   rtmp: {
     port: 1935,
     chunk_size: 20000,
     gop_cache: true,
     ping: 30,
-    ping_timeout: 60
+    ping_timeout: 60,
+    auth_callback: checkSessionShit
   },
   relay: {
     ffmpeg: '/usr/local/bin/ffmpeg',
@@ -16,7 +50,7 @@ const config = {
       {
         app: 'live',
         mode: 'push',
-        edge: 'rtmp://10.1.0.148:8000/edge',
+        edge: process.env.TRANSCODER_IP,
       }
     ]
   },
@@ -43,15 +77,6 @@ nms.on('prePublish', async (id, StreamPath, args) => {
     "server_ip": process.env.server_ip,
     "channel": StreamPath.replace('/live/', ''),
     "submitted_key": args.auth
-  }
-
-  try {
-    const ingestAuthResponse = (await axios.post('http://api-tunnel.brime.tv/v1/streams/ingest-auth', body)).data;
-
-    // Do whatever the fuck you want.
-  } catch (e) {
-    const session = nms.getSession(id);
-    session.reject();
   }
 });
 
